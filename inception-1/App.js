d2b0a98934c51ff94495e2f92a20d8de33a5a3a2
Mitali Bhoir <mitu.190312@gmail.com>
2023-03-25 20:02:05 +0530

    Inception: Nested HTML Tags having single child.

--- inception-1/App.js
+++ inception-1/App.js
@@ -1,4 +1,30 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
+/**
+ * <div id="parent">
+ *     <div id="child">
+ *        <h1>I am h1 tag!</h1>
+ *   </div>
+ * </div>
+ *
+ * React element is and Object. render() converts it into HTML which is understandable by Browser.
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading2" }, "I am h1 tag!")
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+
+// root.render(heading);
