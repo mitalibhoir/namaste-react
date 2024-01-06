2792e0c52e7a4f64468dfda29d590adb51ece874
Mitali Bhoir <mitu.190312@gmail.com>
2024-01-06 17:08:48 +0530

    Episode 2 - Git and npm basics with parcel bundler installation

---
+++ 02-igniting-app/App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "hh" }, "I am a h1 tag!"),
+    React.createElement("h2", { key: "yy" }, "I am a h2 tag!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { key: "hh" }, "I am a h1 tag!"),
+    React.createElement("h2", { key: "yy" }, "I am a h2 tag!"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
